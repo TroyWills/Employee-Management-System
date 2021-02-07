f972e628c2b47b0ec0a874d07e9c009748ef108e
Troy Wills <troy_wills@outlook.com>
2021-02-07 17:14:08 -0600

    progress

--- 02-Homework/Employees.js
+++ 02-Homework/Employees.js
@@ -18,11 +18,9 @@ inquirer
         message: "What position for new employee?",
         choices: ["Owner", "Sales", "Marketing", "Human Resources", "Operations", "Engineer"],
     },
-
-
   ])
   .then((answer) => {
     console.log("Welcome New Employee " + answer.first_name, answer.last_name);
-    console.log("Title: " + answer.position)
+    console.log("Title: " + answer.title)
   });
 
